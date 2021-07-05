d8c1c1faa05ad2218f05d5eb64f43c2b0ca7ca67
admin <reveval95@gmail.com>
2021-07-05 14:20:40 +0300

    feat: implement data retrieval in InformationFragment.kt

--- weatherappmodule/src/main/java/by/mbicycle/develop/weatherappmodule/ui/city/InformationFragment.kt
+++ weatherappmodule/src/main/java/by/mbicycle/develop/weatherappmodule/ui/city/InformationFragment.kt
@@ -5,39 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
+import androidx.fragment.app.setFragmentResultListener
 import by.mbicycle.develop.weatherappmodule.BottomBarVisibilityListener
-import by.mbicycle.develop.weatherappmodule.R
-import by.mbicycle.develop.weatherappmodule.databinding.InformationFragmentBinding
+import by.mbicycle.develop.weatherappmodule.REQUEST_KEY
+import by.mbicycle.develop.weatherappmodule.databinding.FragmentInformationBinding
 import by.mbicycle.develop.weatherappmodule.ui.city.models.WeatherItem
 
 class InformationFragment : Fragment() {
-    private lateinit var binding: InformationFragmentBinding
+    private lateinit var binding: FragmentInformationBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = InformationFragmentBinding.inflate(inflater, container, false)
+        binding = FragmentInformationBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        childFragmentManager.commit {
-            add<InformationFragment>(R.id.root_city_fragment_container)
-        }
-
         activity?.let {
             if (it is BottomBarVisibilityListener) {
                 it.setBottomBarVisibility(View.GONE)
             }
         }
+
+        setFragmentResultListener(REQUEST_KEY) { _, bundle ->
+            val result = bundle.getParcelable<WeatherItem>("data")
+            result?.let {
+                setSelectedItem(it)
+            }
+        }
+
+        binding.buttonBackStack.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
     }
 
-    fun setSelectedItem(item: WeatherItem) {
+    private fun setSelectedItem(item: WeatherItem) {
         binding.apply {
             cityName.text = item.cityName
             temperatureTextView.text = item.temperature.toString()

6aff9e129138a927c8dff954040a4ac283d0c6c9
admin <reveval95@gmail.com>
2021-07-05 17:46:03 +0300

    feat: implement date-handler and set formatted date into WeatherItem model

--- weatherappmodule/src/main/java/by/mbicycle/develop/weatherappmodule/Extensions.kt
+++ weatherappmodule/src/main/java/by/mbicycle/develop/weatherappmodule/Extensions.kt
@@ -4,12 +4,19 @@ import by.mbicycle.develop.weatherappmodule.ui.city.models.LocationModel
 import by.mbicycle.develop.weatherappmodule.ui.city.models.WeatherItem
 import by.mbicycle.develop.weatherappmodule.ui.city.models.WeatherModelForFixedList
 import by.mbicycle.develop.weatherappmodule.ui.city.models.WeatherModelForSearchList
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun WeatherModelForSearchList.mapToItem(locations: List<LocationModel>) : WeatherItem {
     val cityName = locations.filter { link.contains(it.cityKey) }.first().cityName
-    return WeatherItem(cityName, weatherIconId, temperature.metric.value.toInt())
+    val date = Date(epochTime * 1000L)
+    val dateWithPattern = SimpleDateFormat(PATTERN_FOR_DATE_FORMAT, Locale.ENGLISH).format(date)
+    return WeatherItem(cityName, weatherIconId, temperature.metric.value.toInt(), dateWithPattern)
 }
 
 fun WeatherModelForFixedList.mapToItem() : WeatherItem {
-    return WeatherItem(cityName, weatherDetails.first().weatherId, main.temperature.toInt())
+    val date = Date(date * 1000L)
+    val dateWithPatten = SimpleDateFormat(PATTERN_FOR_DATE_FORMAT, Locale.ENGLISH).format(date)
+    return WeatherItem(cityName, weatherDetails.first().weatherId, main.temperature.toInt(),
+        dateWithPatten)
 }

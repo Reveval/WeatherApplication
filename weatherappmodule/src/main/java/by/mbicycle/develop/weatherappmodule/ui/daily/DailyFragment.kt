4114ed9910e357e36549b674bd8c592f4fc99c86
admin <reveval95@gmail.com>
2021-07-07 17:04:07 +0300

    feat: implement DailyFragment class

--- weatherappmodule/src/main/java/by/mbicycle/develop/weatherappmodule/ui/daily/DailyFragment.kt
+++ weatherappmodule/src/main/java/by/mbicycle/develop/weatherappmodule/ui/daily/DailyFragment.kt
@@ -1,30 +1,110 @@
 package by.mbicycle.develop.weatherappmodule.ui.daily
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import by.mbicycle.develop.weatherappmodule.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import by.mbicycle.develop.weatherappmodule.CurrentLocation
+import by.mbicycle.develop.weatherappmodule.databinding.FragmentDailyBinding
+import by.mbicycle.develop.weatherappmodule.mapToModel
 
 class DailyFragment : Fragment() {
+    private lateinit var binding: FragmentDailyBinding
+    private lateinit var retrofitManager: RetrofitManagerForDailyForecast
+    private lateinit var recyclerAdapter: RecyclerAdapterForDailyTab
+    private var location: Location? = null
+    private var listOfDailyForecastItems = arrayListOf<DailyForecastItem>()
+
+    private val requestLocationPermission = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            showDailyForecast()
+        } else {
+            showMessageNoData()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        retrofitManager = RetrofitManagerForDailyForecast()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_daily, container, false)
+    ): View {
+        binding = FragmentDailyBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onStart() {
-        super.onStart()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        listOfDailyForecastItems.clear()
+
+        recyclerAdapter = RecyclerAdapterForDailyTab()
+
+        binding.recyclerForDailyForecast.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = recyclerAdapter
+            overScrollMode = View.OVER_SCROLL_NEVER
+        }
+
+        context?.let { it ->
+            if (ContextCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+                showDailyForecast()
+            } else {
+                requestLocationPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
+    private fun getLocationData() : Location? {
+        return context?.let { CurrentLocation(it).getLastKnownLocation() }
+    }
+
+    private fun showMessageNoData() {
+        binding.apply {
+            groupMessageNoData.visibility = View.VISIBLE
+            buttonAllowAccess.setOnClickListener {
+                requestLocationPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+
+    private fun showDailyForecast() {
+        binding.groupMessageNoData.visibility = View.GONE
+        location = getLocationData()
+        location?.let {
+
+            retrofitManager.getCityName(it.latitude, it.longitude) { model ->
+
+                Handler(Looper.getMainLooper()).post {
+                    binding.cityNameTextView.text = model.cityName
+                }
+            }
+
+            retrofitManager.getDailyForecast(it.latitude, it.longitude) { modelApi ->
+                modelApi.listOfDailyForecast.forEach { daily ->
+                    val item = daily.mapToModel()
+                    listOfDailyForecastItems.add(item)
+                }
+
+                Handler(Looper.getMainLooper()).post {
+                    recyclerAdapter.setData(listOfDailyForecastItems)
+                }
+            }
+        }
     }
 }
